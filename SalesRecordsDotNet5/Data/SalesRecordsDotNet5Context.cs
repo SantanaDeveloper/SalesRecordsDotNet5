6a20aa99d5e2b68f3f28b281263b6244ec2167ae
João Paulo Santana <santanadeveloper@outlook.com>
2021-01-02 21:11:43 -0300

    [Add] Departments Scaffolding

---
+++ SalesRecordsDotNet5/Data/SalesRecordsDotNet5Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesRecordsDotNet5.Models;
+
+namespace SalesRecordsDotNet5.Data
+{
+    public class SalesRecordsDotNet5Context : DbContext
+    {
+        public SalesRecordsDotNet5Context (DbContextOptions<SalesRecordsDotNet5Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesRecordsDotNet5.Models.Department> Department { get; set; }
+    }
+}
