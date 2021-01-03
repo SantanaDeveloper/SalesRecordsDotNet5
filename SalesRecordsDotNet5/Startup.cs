cf07dcf6c66a8dce0e20aa347601a9a0904e7006
João Paulo Santana <santanadeveloper@outlook.com>
2021-01-03 11:07:12 -0300

    [Add] Seeding Service

--- SalesRecordsDotNet5/Startup.cs
+++ SalesRecordsDotNet5/Startup.cs
@@ -37,14 +37,16 @@ namespace SalesRecordsDotNet5
                 .EnableSensitiveDataLogging()
                 .EnableDetailedErrors()
                 );
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
