2fa9bd74e84ea5d071a19ed577783bbae2a2c7e5
João Paulo Santana <santanadeveloper@outlook.com>
2021-01-02 22:54:15 -0300

    [Add] Other entities and second migration.

--- SalesRecordsDotNet5/Data/SalesRecordsDotNet5Context.cs
+++ SalesRecordsDotNet5/Data/SalesRecordsDotNet5Context.cs
@@ -14,6 +14,8 @@ namespace SalesRecordsDotNet5.Data
         {
         }
 
-        public DbSet<SalesRecordsDotNet5.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+        public DbSet<Seller> Seller { get; set; }
     }
 }
---
+++ SalesRecordsDotNet5/Models/Enums/SaleStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesRecordsDotNet5.Models.Enums
+{
+    public enum SaleStatus : int
+    {
+        Pending = 0,
+        Billed,
+        Canceled
+    }
+}
