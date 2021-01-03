08988a055715133b5610de0f91a0832ee615bb1f
João Paulo Santana <santanadeveloper@outlook.com>
2021-01-03 19:25:26 -0300

    [Change] Methods Async and referential integrity

--- SalesRecordsDotNet5/Services/DepartmentService.cs
+++ SalesRecordsDotNet5/Services/DepartmentService.cs
@@ -1,8 +1,10 @@
-﻿using SalesRecordsDotNet5.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesRecordsDotNet5.Data;
 using SalesRecordsDotNet5.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace SalesRecordsDotNet5.Services
 {
@@ -15,9 +17,9 @@ namespace SalesRecordsDotNet5.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
